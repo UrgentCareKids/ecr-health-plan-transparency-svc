0b46040684ca98000b9a450cdce24e9fcba45f6b
smaxwell <smaxwell@expressotech.com>
2023-09-19 19:35:23 -0400

    changed to SSM, added call parameters for bucket, file name etc

--- src/app.py
+++ src/app.py
@@ -3,9 +3,23 @@ import psycopg2
 import psycopg2.extras
 import ijson
 import boto3
+import sys
 from ijson.common import ObjectBuilder
 from decimal import *
 from datetime import datetime
+
+# Get and check for existence the passed parameters
+# exit if all 3 are not passed
+if sys.argv[0] is None or sys.argv[1] is None or sys.argv[2] is None:
+    print(f"Please call with 3 parameters (bucket_name,file_path,file_name)")
+    sys.exit()
+else:
+    bucket_name = sys.argv[1]
+    print(f"bucket_name : {bucket_name}")
+    file_path = sys.argv[2]
+    print(f"file_path : {file_path}")
+    file_name = sys.argv[3]
+    print(f"file_name : {file_name}")
 
 class DecimalEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -20,8 +34,8 @@ class DecimalEncoder(json.JSONEncoder):
 #ssm = boto3.client('ssm',  aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'], aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'],  region_name='us-east-2')
 ssm = boto3.client('ssm')
 # local dev
-param = ssm.get_parameter(Name='db_postgres_local_scott', WithDecryption=True )
-#param = ssm.get_parameter(Name='db_postgres_transparency_svc', WithDecryption=True )
+#param = ssm.get_parameter(Name='db_postgres_local_scott', WithDecryption=True )
+param = ssm.get_parameter(Name='db_postgres_transparency_svc', WithDecryption=True )
 params_request = json.loads(param['Parameter']['Value']) 
 
 def postgres_conn():
@@ -51,37 +65,15 @@ cursor = targetconnection.cursor()
 # Initialize AWS S3 client
 #s3 = boto3.client('s3',  aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'], aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'],  region_name='us-east-2'))
 s3 = boto3.client('s3')
-bucket_name = 'uc4k-db'
-file_name = 'data/public-data-files/transparency-index/2023-08-01_anthem_index.json'
+#bucket_name = 'uc4k-db'
+#file_name = 'data/public-data-files/transparency-index/2023-08-01_anthem_index.json'
 local_path = '/mount/datastorage'
 # pull S3 file to local file
-s3.download_file(bucket_name,file_name,local_path + '/' + '2023-08-01_anthem_index.json')
+# testing commented section 1
+#s3.download_file(bucket_name,file_name,local_path + '/' + '2023-08-01_anthem_index.json')
+s3.download_file(bucket_name,file_path+file_name,local_path + '/' + file_name)
 print('Local file created : ' + file_name,datetime.now())
-in_network = (local_path + '/' + '2023-08-01_anthem_index.json')
-
-#in_network("../../gsh_transparency_parser_python/data/2023-08-01_anthem_index.json")
-#2 NO
-# S3   do i have to download?  s3.download_file(bucket_name,file_key,what do i want to name it ie. in_network)
-#s3_obj = s3.get_object(Bucket='uc4k-db', Key='/data/public-data-files/transparency-index/2023-08-01_anthem_index.json')
-#in_network = s3_obj['Body'].read()
-# 2  NO
-# 3 NO
-#def read_file_from_s3(bucket_name, file_name):
-#    obj = s3.get_object(Bucket=bucket_name, Key=file_name)
-#    data = obj['Body'].read()
-#    return data
-# 3 NO
-# https://stackoverflow.com/questions/70913017/how-to-read-content-of-a-file-from-a-folder-in-s3-bucket-using-python
-# 4
-#response = s3.get_object(Bucket=bucket_name, Key=file_name)
-#bytes = response['Body'].read()  # returns bytes since Python 3.6+
-#in_network = json.loads(bytes['Body'].read().decode('utf-8')) #original got error
-#in_network = json.loads(str['Body'].read().decode('utf-8'))
-#  error text
-    #File "/mnt/c/Users/smaxw/OneDrive/work/UrgentCare4Kids/Git/ecr-health-plan-transparency-svc/src/app.py", line 73, in <module>
-    #in_network = json.loads(bytes['Body'].read().decode('utf-8'))
-    #TypeError: byte indices must be integers or slices, not str
-#  error text
+# testing commented section 1
 
 def in_network(json_filename):
     print('Starting in_network import. ',datetime.now())
@@ -109,6 +101,9 @@ def in_network(json_filename):
     print('in_network complete. ',datetime.now())
          #   print(dict)
          #print(json.dumps(dict, cls=DecimalEncoder))
+
+in_network(local_path + '/' + '2023-08-01_anthem_index.json')
+#in_network("/mount/datastorage/2023-08-01_anthem_index.json")   # fully resolved
 
 # for key, value in objects(open('./data/2022-07-01_DIAMONDHEAD-URGENT-CARE-LLC_PS1-50_C2_in-network-rates.json', 'rb')):
 #    print(key, ' ', value)
